a23414999c6fe4ae85ffb63c643650887e5c1c91
Malachy Gilchrist <malachygilchrist@gmail.com>
2018-11-02 15:40:30 +0000

    test: weather conditions tests pass

--- lib/airport.rb
+++ lib/airport.rb
@@ -4,11 +4,19 @@ class Airport
   end
 
   def store(plane)
+    raise 'Stormy Weather, unsafe to land' if weather == 'stormy'
     @hangar.push(plane)
   end
 
   def remove(plane)
+    raise 'Stormy Weather, unsafe to take off' if weather == 'stormy'
     @hangar.delete(plane)
     @hangar
+  end
+
+  private
+
+  def weather
+    rand() < 0.8 ?  'clear' : 'stormy'
   end
 end
