5a23b20d6bc885e78a3e93a4baa8179e69b2fdc5
Malachy Gilchrist <malachygilchrist@gmail.com>
2018-11-02 15:31:31 +0000

    test: weather status effect on airport
    original tests refactored to stub random weather conditions, two more
    tests added to check airport hangar doesn't change state during stormy
    weather

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,18 +2,41 @@ require 'airport'
 
 describe Airport do
   let(:mockPlane) { double :aeroplane }
-  describe '.store' do
-    it 'stores provided argument in its hangar' do
-      expect(subject.store(mockPlane))
-        .to include(mockPlane)
+  
+  context 'when weather is clear' do
+    before(:each) do
+      allow(subject).to receive(:weather) { "clear" }
+    end
+
+    describe '.store' do
+      it 'stores provided argument in its hangar' do
+        expect(subject.store(mockPlane))
+          .to include(mockPlane)
+      end
+    end
+
+    describe '.remove' do
+      it 'removes provided argument from the hangar' do
+        subject.store(mockPlane)
+        expect(subject.remove(mockPlane))
+          .not_to include(mockPlane)
+      end
     end
   end
 
-  describe '.remove' do
-    it 'removes provided argument from the hangar' do
-      subject.store(mockPlane)
-      expect(subject.remove(mockPlane))
-        .not_to include(mockPlane)
+  context 'when weather is stormy' do
+    before(:each) do
+      allow(subject).to receive(:weather) { "stormy" }
+    end
+
+    it 'prevents planes from landing' do
+      expect { subject.store(mockPlane) }
+        .to raise_error "Stormy Weather, unsafe to land"
+    end
+
+    it 'prevents planes from taking off' do
+      expect { subject.remove(mockPlane) }
+        .to raise_error "Stormy Weather, unsafe to take off"
     end
   end
 end
