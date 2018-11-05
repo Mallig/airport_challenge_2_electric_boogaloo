e1fab07b1510f5e9e0d56493e7b9abac649d0572
Malachy Gilchrist <malachygilchrist@gmail.com>
2018-11-05 10:57:50 +0000

    test: tests for plane presence in hangar
    expects errors to be raise if planes are stored in the hangar when
    already present. expects errors if plane is remove from hangar when it
    isn't there

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -9,18 +9,36 @@ describe Airport do
     end
 
     context 'when airport is not full' do
-      describe '.store' do
-        it 'stores provided argument in its hangar' do
-          expect(subject.store(mockPlane))
-            .to include(mockPlane)
+      context 'when plane is in the hangar' do
+        describe '.store' do
+          it 'raises an error' do
+            expect { subject.store(mockPlane) }
+              .to raise_error "Plane already in hangar"
+          end
+        end
+
+        describe '.remove' do
+          it 'removes the plane from the hangar' do
+            subject.store(mockPlane)
+            expect(subject.remove(mockPlane))
+              .not_to include(mockPlane)
+          end
         end
       end
 
-      describe '.remove' do
-        it 'removes provided argument from the hangar' do
-          subject.store(mockPlane)
-          expect(subject.remove(mockPlane))
-            .not_to include(mockPlane)
+      context 'when plane is not in hangar' do
+        describe '.store' do
+          it 'stores the plane in its hangar' do
+            expect(subject.store(mockPlane))
+              .to include(mockPlane)
+          end
+        end
+
+        describe '.remove' do
+          it 'raises an error' do
+            expect { subject.remove(mockPlane) }
+              .to raise_error "Plane not in hangar"
+          end
         end
       end
     end
