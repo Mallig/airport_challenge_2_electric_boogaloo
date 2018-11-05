52a837d33ffe4dcdcb63c0062a503cf3188dccb2
Malachy Gilchrist <malachygilchrist@gmail.com>
2018-11-05 12:58:28 +0000

    test: plane raises errors

--- lib/aeroplane.rb
+++ lib/aeroplane.rb
@@ -4,12 +4,14 @@ class Aeroplane
   end
 
   def land(airport)
+    raise 'Plane has already landed' if @landed
     airport.store(self)
     @landed = true
     "plane has landed at airport"
   end
 
   def takeoff(airport)
+    raise 'Plane is already in the air' unless @landed
     airport.remove(self)
     @landed = false
     "plane has left airport"
